15eeab6df53e71e8681aa5f3ac71bbd92a7ccdb7
KostyaTurchenko <34889744+KostyaTurchenko@users.noreply.github.com>
2018-09-25 00:12:06 +0300

    Sequences
    task2

---
+++ Sequences/BL/SequenceCalculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BL
+{
+    public class SequenceCalculator
+    {
+        public double X { get; set; }
+        public int N { get; set; }
+
+        public double Fault { get; private set; }
+
+        public SequenceCalculator(double X, int N)
+        {
+            this.X = X;
+            this.N = N;
+        }
+
+        private int Factorial(int numb)
+        {
+            int result = 1;
+            for (int i = numb; i > 1; i--)
+                result *= i;
+            return result;
+        }
+
+
+        public double GetResult ()
+        {
+            double sum = 0;
+            for (int i = 0; i < N; i++)
+            {
+                sum += Math.Pow(-1, i) * Math.Pow(X, i) / Factorial(i);
+            }
+
+            Fault = Math.Abs(Math.Pow(Math.E, -X) - sum);
+
+            return sum;
+        }
+
+        public double CompareFaultAndLast() => Math.Abs(Fault - Math.Pow(-1, N) * Math.Pow(X, N) / Factorial(N));
+
+
+    }
+}
---
+++ Sequences/task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BL;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            SequenceCalculator MyCalc = new SequenceCalculator(x, n);
+
+            Console.WriteLine(MyCalc.GetResult());
+            Console.WriteLine("Погрешность: {0}", MyCalc.Fault);
+            Console.WriteLine("Разница между погрешностью и N-го слагаемого: {0}", MyCalc.CompareFaultAndLast());
+            Console.WriteLine();
+            Console.WriteLine("Значение e^(-{0}): {1}",x, Math.Pow(Math.E, -x));
+
+        }
+    }
+}
